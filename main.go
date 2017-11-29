229c3ad115b4354a242b8e1c2191edcf4fb4cce5
miao <miao@miao-All-Series>
2017-11-29 09:47:56 +0800

    11.29

--- main.go
+++ main.go
@@ -5,9 +5,6 @@ import (
 	"io"
 	"net/http"
 	"strings"
-	"time"
-
-	"github.com/seefan/to"
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
@@ -101,34 +98,39 @@ func main() {
 	//qq.Main()
 	//编译模块
 	//fmt.Println(share.Add(2, 3))
-	d := "2017-11-06 18:57"
-	e := "2018-02-06 18:57"
-	fmt.Println("d>e", d > e)
-	a := "18"
-	b := "19"
-	fmt.Println(a > b)
+	/*
+		//做约单时候留下的
+			d := "2017-11-06 18:57"
+			e := "2018-02-06 18:57"
+			fmt.Println("d>e", d > e)
+			a := "18"
+			b := "19"
+			fmt.Println(a > b)
 
-	fmt.Println("使劲", time.Now().Month())
-	fmt.Println(time.Now().Unix())
-	//sd := "2017-11-10 19:07"
-	//获取本地location
-	toBeCharge := "2017-11-11 00:02:00"                             //待转化为时间戳的字符串 注意 这里的小时和分钟还要秒必须写 因为是跟着模板走的 修改模板的话也可以不写
-	timeLayout := "2006-01-02 15:04:05"                             //转化所需模板
-	loc, _ := time.LoadLocation("Local")                            //重要：获取时区
-	theTime, _ := time.ParseInLocation(timeLayout, toBeCharge, loc) //使用模板在对应时区转化为time.time类型
-	sr := theTime.Unix()                                            //转化为时间戳 类型是int64
-	fmt.Println(theTime)                                            //打印输出theTime 2015-01-01 15:15:00 +0800 CST
-	fmt.Println(sr)
-	fmt.Println(int(theTime.Weekday()))
-	if 0 == theTime.Weekday() {
-		fmt.Println(" 啊")
-	}
-	//
-	sdd := time.Now().UTC()
-	fmt.Println(to.String(sdd)[0:10])
-	fmt.Println("ss", getTimeString("2017-11-16 0:00"))
-	fmt.Println(time.Now().Unix())
-	fmt.Println(time.Now().Nanosecond())
+			fmt.Println("使劲", time.Now().Month())
+			fmt.Println(time.Now().Unix())
+			//sd := "2017-11-10 19:07"
+			//获取本地location
+			toBeCharge := "2017-11-11 00:02:00"                             //待转化为时间戳的字符串 注意 这里的小时和分钟还要秒必须写 因为是跟着模板走的 修改模板的话也可以不写
+			timeLayout := "2006-01-02 15:04:05"                             //转化所需模板
+			loc, _ := time.LoadLocation("Local")                            //重要：获取时区
+			theTime, _ := time.ParseInLocation(timeLayout, toBeCharge, loc) //使用模板在对应时区转化为time.time类型
+			sr := theTime.Unix()                                            //转化为时间戳 类型是int64
+			fmt.Println(theTime)                                            //打印输出theTime 2015-01-01 15:15:00 +0800 CST
+			fmt.Println(sr)
+			fmt.Println(int(theTime.Weekday()))
+			if 0 == theTime.Weekday() {
+				fmt.Println(" 啊")
+			}
+			//
+			sdd := time.Now().UTC()
+			fmt.Println(to.String(sdd)[0:10])
+			fmt.Println("ss", getTimeString("2017-11-16 0:00"))
+			fmt.Println(time.Now().Unix())
+			fmt.Println(time.Now().Nanosecond())
+
+	*/
+
 }
 func getTimeString(s string) string {
 	if len(s) < 16 {
