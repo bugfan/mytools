5603595dc66734b95a27c10ef9034efd49f15625
zhaoxinyang <908958194@qq.com>
2019-05-14 17:39:44 +0800

    适配器模式用于转换一种接口适配另一种接口。
    实际使用中Adaptee一般为接口，并且使用工厂函数生成实例。
    
    在Adapter中匿名组合Adaptee接口，所以Adapter类也拥有SpecificRequest实例方法，又因为Go语言中非入侵式接口特征，其实Adapter也适配Adaptee接口。

---
+++ design/2_adapter/adapter_test.go
@@ -0,0 +1,16 @@
+package adapter
+
+import "testing"
+
+var expect = "adaptee method"
+
+func TestAdapter(t *testing.T) {
+	adaptee := NewAdaptee()
+	target := NewAdapter(adaptee)
+	res := target.Request()
+	if res != expect {
+		t.Fatalf("expect: %s, actual: %s", expect, res)
+	}
+}
+
+// none
