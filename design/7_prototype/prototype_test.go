6949201f5fc64edd11bcc5c76f840073cbd522d9
zhaoxinyang <908958194@qq.com>
2019-05-17 17:03:56 +0800

    原型模式使对象能复制自身，并且暴露到接口中，使客户端面向接口编程时，不知道接口实际对象的情况下生成新的对象。
    原型模式配合原型管理器使用，使得客户端在不知道具体类的情况下，通过接口管理器得到新的实例，并且包含部分预设定配置。
    
    感觉这个还有点意思，这个可以很快的把一些数据库的对象转成实例，

---
+++ design/7_prototype/prototype_test.go
@@ -0,0 +1,53 @@
+package prototype
+
+import "testing"
+
+var manager *PrototypeManager
+
+type Type1 struct {
+	name string
+}
+
+func (t *Type1) Clone() Cloneable {
+	tc := *t
+	return &tc
+}
+
+type Type2 struct {
+	name string
+}
+
+func (t *Type2) Clone() Cloneable {
+	tc := *t
+	return &tc
+}
+
+func TestClone(t *testing.T) {
+	t1 := manager.Get("t1")
+
+	t2 := t1.Clone()
+
+	if t1 == t2 {
+		t.Fatal("error! get clone not working")
+	}
+}
+
+func TestCloneFromManager(t *testing.T) {
+	c := manager.Get("t1").Clone()
+
+	// 我tm的竟然忘了接口可以断言成为类
+	t1 := c.(*Type1)
+	if t1.name != "type1" {
+		t.Fatal("error")
+	}
+
+}
+
+func init() {
+	manager = NewPrototypeManager()
+
+	t1 := &Type1{
+		name: "type1",
+	}
+	manager.Set("t1", t1)
+}
