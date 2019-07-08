0e947baa65446f98d2b6cf3f2cbc624ea56f29af
Fan <30284615+bugfan@users.noreply.github.com>
2019-07-08 14:36:51 +0800

    Create 飘窗，广告.js

---
+++ js-catch/飘窗，广告.js
@@ -0,0 +1,20 @@
+<script>alert(44)</script>
+<div style="position:fixed; top:50%; left:50%;">
+<img src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=180868167,273146879&fm=27&gp=0.jpg"/>
+</div>
+我的
+
+
+<div id="img" style="position:fixed;right:10px;top:50%;">
+      < img src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1926707095,4273239503&fm=26&gp=0.jpg"/>
+      <div style="position:absolute;z-index: 1;right: 0;top:0;" onclick="shutdown()">
+        关闭
+      </div>
+   </div>
+  <script type="text/javascript">
+    function shutdown(){
+      var img = document.getElementById("img")
+      img.style.display = "none"
+    }
+  </script>
+  春良
