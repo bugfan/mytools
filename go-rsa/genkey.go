582a511ef06420a2441c8fa1528a46233937eda7
Fan <30284615+bugfan@users.noreply.github.com>
2019-08-06 18:02:54 +0800

    golang rsa 加密 解密

---
+++ go-rsa/genkey.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"flag"
+	"log"
+	"os"
+)
+
+func main() {
+	var bits int
+	flag.IntVar(&bits, "b", 1024, "秘钥长度，默认为1024")
+	if err := GenRsaKey(bits); err != nil {
+		log.Fatal("秘钥文件生成失败")
+	}
+	log.Println("秘钥文件生成成功")
+}
+
+//生成 私钥和公钥文件
+func GenRsaKey(bits int) error {
+	//生成私钥文件
+	privateKey, err := rsa.GenerateKey(rand.Reader, bits)
+	if err != nil {
+		return err
+	}
+	derStream := x509.MarshalPKCS1PrivateKey(privateKey)
+	block := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: derStream,
+	}
+	file, err := os.Create("private.pem")
+	if err != nil {
+		return err
+	}
+	err = pem.Encode(file, block)
+	if err != nil {
+		return err
+	}
+	//生成公钥文件
+	publicKey := &privateKey.PublicKey
+	defPkix, err := x509.MarshalPKIXPublicKey(publicKey)
+	if err != nil {
+		return err
+	}
+	block = &pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: defPkix,
+	}
+	file, err = os.Create("public.pem")
+	if err != nil {
+		return err
+	}
+	err = pem.Encode(file, block)
+	if err != nil {
+		return err
+	}
+	return nil
+}
