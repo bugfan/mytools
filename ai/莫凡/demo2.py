71d959a763145bffce0c5b1cdbe0a9c44d1b1feb
bugfan <908958194@qq.com>
2020-01-07 11:56:47 +0800

    fix: 跑起来了,自带gui

--- ai/莫凡/demo2.py
+++ ai/莫凡/demo2.py
@@ -7,14 +7,12 @@ loaded_data = datasets.load_boston()
 data_X = loaded_data.data
 data_y = loaded_data.target
 
+model = LinearRegression()
+model.fit(data_X, data_y)
+
 print(model.predict(data_X[:4, :]))
 print(data_y[:4])
 
-X, y = datasets.make_regression(n_samples=100, n_features=1, n_targets=1, noise=10)
-
-plt.scatter(X, y)
-plt.show()
-
-X, y = datasets.make_regression(n_samples=100, n_features=1, n_targets=1, noise=50)
+X, y = datasets.make_regression(n_samples=100, n_features=1, n_targets=1, noise=20)
 plt.scatter(X, y)
 plt.show()
