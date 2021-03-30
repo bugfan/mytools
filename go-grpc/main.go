b27a50bf9bd9f39e185fdde4c0a7873eddfddb46
bugfan <908958194@qq.com>
2021-03-30 15:25:51 +0800

    grpc

--- go-grpc/main.go
+++ go-grpc/main.go
@@ -1,5 +1,30 @@
 package main
-
+  
+import (
+    "log"
+    "net"
+  
+    "golang.org/x/net/context"
+    "google.golang.org/grpc"
+    pb "helloworld/helloworld"
+)
+  
+const (
+    port = ":50051"
+)
+  
+type server struct {}
+  
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+    return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+}
+  
 func main() {
-	
+    lis, err := net.Listen("tcp", port)
+    if err != nil {
+        log.Fatal("failed to listen: %v", err)
+    }
+    s := grpc.NewServer()
+    pb.RegisterGreeterServer(s, &server{})
+    s.Serve(lis)
 }
